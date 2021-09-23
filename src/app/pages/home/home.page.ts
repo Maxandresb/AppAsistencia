5875e3ab784b2ab2de59d03f0fe0f370695637f7
Benja <be.gatica@duocuc.cl>
2021-09-22 21:34:16 -0300

    Cambios estilo

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -8,6 +8,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomePage {
   
+  asistencia=[{
+    asignatura:'Programacion de Aplicaciones Moviles',
+    codigo:'PGY-4121',
+    porcentaje:'100% (10 de 10 clases)'
+  },
+  {
+    asignatura:'Calidad de Software',
+    codigo:'CSY-41111',
+    porcentaje:'80% (8 de 10 clases)'
+  },
+  {
+    asignatura:'Arquitectura',
+    codigo:'ASY-4131',
+    porcentaje:'100% (10 de 10 clases)'
+  },
+ {
+  asignatura:'Inglés',
+  codigo:'INI-5111',
+  porcentaje:'60% (6 de 10 clases)'
+
+}]
   nombreusuario:string;
   constructor(private activeroute:ActivatedRoute, private router:Router) {
     
